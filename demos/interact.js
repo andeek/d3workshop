c57730cfcda6d80472334682b3b9e4a09f7c0795
Andee Kaplan <ajkaplan@iastate.edu>
2016-04-13 17:56:20 -0500

    update many typos

--- demos/interact.js
+++ demos/interact.js
@@ -1,24 +1,29 @@
-var svg = d3.select(".interact").append("svg")
-    .attr("height", 50);
+var interaction_chart = function(selection) {
+  selection.each(function(d, i) {
+    var svg = d3.select(this).append("svg")
+        .attr("height", 50);
 
-var circle = svg.append("circle")
-    .attr("cx", 15)
-    .attr("cy", 30)
-    .attr("r", 15)
-    .attr("fill", "red");  
+    var circle = svg.append("circle")
+        .attr("cx", 15)
+        .attr("cy", 30)
+        .attr("r", 15)
+        .attr("fill", "red");  
 
-circle.on("mouseover", function() {
-      d3.select(this).transition().duration(300)
-          .style("fill", "yellow");
-    })
-    .on("mouseout", function(){
-      d3.select(this).transition().duration(300)
-          .style("fill", "red");
-    })
-    .on("click", function() {
-      alert("Hello!");
-    });
-    
-circle.append("title")
-    .text("A trick!");
+    circle.on("mouseover", function() {
+          d3.select(this).transition().duration(300)
+              .style("fill", "yellow");
+        })
+        .on("mouseout", function(){
+          d3.select(this).transition().duration(300)
+              .style("fill", "red");
+        })
+        .on("click", function() {
+          alert("Hello!");
+        });
+        
+    circle.append("title")
+        .text("A trick!");
+  });
+};
+interaction_chart(d3.select(".interact"));
 
--- demos/transition.js
+++ demos/transition.js
@@ -1,51 +1,63 @@
-var color = "red";
+var transitions_chart_1 = function(selection) {
+  selection.each(function(d, i) {
+    var color = "red";
 
-var button = d3.select(".transition")
-    .append("button")
-    .text("Click me!");
+    var button = d3.select(this)
+        .append("button")
+        .text("Click me!");
 
-d3.select(".transition").append("br");
+    d3.select(this).append("br");
 
-var div = d3.select(".transition")
-    .append("div")
-    .style("background-color", "white")
-    .text("Watch me!");
-  
-var transition = function() {
-  div.transition()
-      .style("background-color", color)
-      .duration(2000);
-  color = color == "white" ? "red" : "white";
-}
+    var div = d3.select(this)
+        .append("div")
+        .style("background-color", "white")
+        .text("Watch me!");
+      
+    var transition = function() {
+      div.transition()
+          .style("background-color", color)
+          .duration(2000);
+      color = color == "white" ? "red" : "white";
+    }
 
-button.on("click", transition);
+    button.on("click", transition);
+  });
+};
+transitions_chart_1(d3.select(".transition"));
 
-var loc = 150;
+var transitions_chart_2 = function(selection) {
+  selection.each(function(d, i) {
+    var loc = 150;
 
-var button2 = d3.select(".transition2")
-  .append("button")
-    .text("Click me!");
+    var button2 = d3.select(this)
+      .append("button")
+        .text("Click me!");
 
-d3.select(".transition2").append("br")    
-var svg = d3.select(".transition2")
-  .append("svg");
-    
-var circle = svg.append("circle")
-    .attr("cx", 15)
-    .attr("cy", 30)
-    .attr("r", 15)
-    .style("stroke", "red")
-    .style("fill", "white");
+    d3.select(this).append("br");
+        
+    var svg = d3.select(this)
+      .append("svg");
+        
+    var circle = svg.append("circle")
+        .attr("cx", 15)
+        .attr("cy", 30)
+        .attr("r", 15)
+        .style("stroke", "red")
+        .style("fill", "white");
 
-var transition2 = function() {
-  circle.transition()
-      .attr("cx", loc)
-      .duration(1000)
-      .delay(100);  
-  loc = loc == 15 ? 150 : 15;
-} 
+    var transition2 = function() {
+      circle.transition()
+          .attr("cx", loc)
+          .duration(1000)
+          .delay(100);  
+      loc = loc == 15 ? 150 : 15;
+    } 
 
-button2.on("click", transition2);   
+    button2.on("click", transition2);  
+  });
+}; 
+transitions_chart_2(d3.select(".transition2"));
+
 
     
     
